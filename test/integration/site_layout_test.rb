25b466046d21be1a4f492febc01a2a289ac28d1e
Charles Clemens <cclemensjr@gmail.com>
2018-06-11 06:00:46 -0700

    Create user controller with placeholder sign up page

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -7,8 +7,9 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", root_path,  count: 2
     assert_select "a[href=?]", about_path, count: 2
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", signup_path
 
-    get contact_path
-    assert_select "title", complete_title("Contact")
+    get signup_path
+    assert_select "title", complete_title("Sign up")
   end
 end
