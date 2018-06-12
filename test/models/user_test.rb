6f4314bdf1a0e2547a447b44e4b335a704f84559
Charles Clemens <c@Charless-MacBook-Pro.local>
2018-06-12 13:14:10 -0700

    Add email validation to the db

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -14,6 +14,7 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
+
   test "email should be present" do
     @user.email = "a" * 200 + "@example.com"
     assert_not @user.valid?
@@ -52,5 +53,12 @@ class UserTest < ActiveSupport::TestCase
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
+  end
+
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
   end
 end
