fd06fe38b54ecc065bda2c3fc3d0b76e0e0540eb
Charles Clemens <cclemensjr@gmail.com>
2018-06-16 23:09:09 -0700

    Complete invalid signup test

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "invalid signup information" do
+    #Make sure the signup page can be visited
+    get signup_path
+
+    #Make sure no users are added to the db
+    assert_no_difference "User.count" do
+      #With invalid parameters
+      post users_path, params: { user: { name:                  "",
+                                        email:                 "user@invalid",
+                                        password:              "pass",
+                                        password_confirmation: "word"} }
+    end
+
+    #Render the signup page if submission fails
+    assert_template 'users/new'
+  end
 end
