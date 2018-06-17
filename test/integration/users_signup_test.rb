3fc0d39152274ae50be868dc2ba6274f69ce01fb
Charles Clemens <cclemensjr@gmail.com>
2018-06-17 11:28:04 -0700

    Add test for invalid user registration

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -8,14 +8,21 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 
     #Make sure no users are added to the db
     assert_no_difference "User.count" do
+      #Make sure the signup form is linked to the signup path
+      assert_select 'form[action="/signup"]'
+      
       #With invalid parameters
-      post users_path, params: { user: { name:                  "",
-                                        email:                 "user@invalid",
-                                        password:              "pass",
-                                        password_confirmation: "word"} }
+      post signup_path, params: { user: { name:                  "",
+                                          email:                 "user@invalid",
+                                          password:              "pass",
+                                          password_confirmation: "word"} }
     end
 
     #Render the signup page if submission fails
     assert_template 'users/new'
+    #Make sure there is an element for the error_explanation
+    assert_select 'div#error_explanation'
+    #Make sure there is an element for the actual error message
+    assert_select 'div.field_with_errors'
   end
 end
