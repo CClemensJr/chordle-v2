8db31f51be3d4af9ef5f521749e54bae6e75e816
Charles Clemens <c@Charless-MacBook-Pro.local>
2018-06-19 19:15:58 -0700

    Define the session's current user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,4 +3,9 @@ module SessionsHelper
         # Create a temporary cookie with the session method tied to the user id
         session[:user_id] = user.id
     end
+
+    def current_user
+        # Current_user equals current_user if it is not nil, otherwise it is assigned the value of the find_by method
+        @current_user ||= User.find_by(id: session[:user_id])
+    end
 end
