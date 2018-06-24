12e023d64926bcf5f634fdb316951c375726559f
Charles Clemens <cclemensjr@gmail.com>
2018-06-24 16:26:40 -0700

    Test for successful user profile edit.

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -16,4 +16,21 @@ class UsersEditTest < ActionDispatch::IntegrationTest
 
     assert_template 'users/edit'
   end
+
+  test "succesful edits" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name  = "Ray"
+    email = "ray@liotta.com"
+    patch user_path(@user), params: { user: { name:                  name,
+                                              email:                 email,
+                                              password:              "",
+                                              password_confirmation: "" } }
+
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal: name,  @user.name
+    assert_equal: email, @user.email
+  end
 end
