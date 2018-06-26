5cf99c8d63157d71f5e2d60d99c8e66b73854448
Charles Clemens <cclemensjr@gmail.com>
2018-06-26 04:09:01 -0700

    Update controller to pass correct user test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:show, :edit, :update]
+  before_action :correct_user,   only: [:show, :edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -53,5 +54,11 @@ class UsersController < ApplicationController
         flash[:danger] = "Chordle needs you to log in"
         redirect_to login_url
       end
+    end
+
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless @user == current_user
     end
 end
