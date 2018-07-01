67b87988c471f127a8d212dc53a630de22f958b0
Charles Clemens <cclemensjr@gmail.com>
2018-06-30 17:53:42 -0700

    Test integration of user profile page

---
+++ test/integration/users_profile_page_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+class UsersProfilePageTest < ActionDispatch::IntegrationTest
+  include ApplicationHelper
+
+  def setup
+    @user = users(:ray)
+  end
+
+  test "profile page display" do
+    log_in_as(@user)
+    get user_path(@user)
+    assert_template 'users/show'
+    assert_select 'title', complete_title(@user.name)
+    assert_select 'h1', text: @user.name
+    assert_select 'h1>img.gravatar'
+    assert_match @user.chores.count.to_s, response.body
+    assert_select 'div.pagination'
+    @user.chores.paginate(page: 1).each do |chore|
+      assert_match chore.description, response.body
+    end
+  end
+end
