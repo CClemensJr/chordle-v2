569360f6a20baffc295f19b9886ca66500ec7667
Charles Clemens <cclemensjr@gmail.com>
2018-07-22 16:55:47 -0700

    Refactor active record query

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,24 +8,24 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user   = User.find(params[:id])
-    @chores = @user.chores.paginate(page: params[:page])
-    @chore  = current_user.chores.build
+    @user           = User.find(params[:id])
+    @chores         = @user.chores.paginate(page: params[:page])
+    @chore          = current_user.chores.build
     @available_time = params[:available_time]
+    @chordle_says   = current_user.chores.
     
-
-    if current_user.chores.where(priority: "High").any?
+    #if current_user.chores.where(priority: "High").any?
       #Return a random chore with a priority level of High
-      @chordle_says = current_user.chores.where(["priority = ? and time_to_complete <= ?", "High", "@available_time" ]).sample.description
-    elsif current_user.chores.where(priority: "Medium").any?
+    #  @chordle_says = current_user.chores.where(["priority = ? and time_to_complete <= ?", "High", @available_time]).sample.description
+    #elsif current_user.chores.where(priority: "Medium").any?
       #Return a random chore with a priority level of Medium
-      @chordle_says = current_user.chores.where(["priority = ? and time_to_complete <= ?", "Medium", "@available_time" ]).sample.description
-    elsif current_user.chores.where(["priority = ? and time_to_complete <= ?", "Low", "@available_time" ]).any?
+    #  @chordle_says = current_user.chores.where(["priority = ? and time_to_complete <= ?", "Medium", @available_time ]).sample.description
+    #elsif current_user.chores.where(["priority = ? and time_to_complete <= ?", "Low", @available_time ]).any?
       #Return a random chore with a priority level of Low
-      @chordle_says = current_user.chores.where(priority: "Low").sample.description
-    else
-      @chordle_says = "Give me a chore!"
-    end
+     # @chordle_says = current_user.chores.where(priority: "Low").sample.description
+    #else
+     # @chordle_says = "Give me a chore first!"
+    #end =end
   end
   
 
@@ -73,7 +73,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params[:user].permit(:name, :email, :password, :password_confirmation)
+      params[:user].permit(:name, :email, :password, :password_confirmation, :available_time)
     end
 
 =begin
